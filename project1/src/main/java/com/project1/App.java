172052a69bd3287dafa3f6a0c87e1236b7bf5a90
HP <HP@DESKTOP-OED2DK9>
2024-08-05 23:57:51 +0530

    Initial commit.

---
+++ project1/src/main/java/com/project1/App.java
@@ -0,0 +1,15 @@
+package com.project1;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	App app = new App();
+    	
+        System.out.println( "Hello World!" );
+    }
+}
